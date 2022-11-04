7657001d84a576af45656c622f3269955413bc04
simon-debruijn <simon.debruijn@hotmail.com>
2022-11-04 17:54:48 +0100

    Add code view for debugging

--- src/components/CalendarStep.tsx
+++ src/components/CalendarStep.tsx
@@ -42,19 +42,56 @@ export function CalendarStep() {
   }
 
   return (
-    <>
-      {state.matches("single") ? <h2>Single</h2> : <h2>Multiple</h2>}
-      {state.context.days.length > 0 && (
-        <Days
-          days={state.context.days}
-          onDeleteDay={handleDeleteDay}
-          onChangeStartDate={handleChangeStartDate}
-          onChangeEndDate={handleChangeEndDate}
-          onChangeStartHour={handleChangeStartTime}
-          onChangeEndHour={handleChangeEndTime}
-        />
-      )}
-      <button onClick={handleAddDay}>Add day</button>
-    </>
+    <section
+      style={{
+        display: "flex",
+        flex: 1,
+        width: "100%",
+      }}
+    >
+      <section
+        style={{
+          position: "relative",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          width: "50%",
+        }}
+      >
+        {state.matches("single") ? <h2>Single</h2> : <h2>Multiple</h2>}
+        {state.context.days.length > 0 && (
+          <Days
+            days={state.context.days}
+            onDeleteDay={handleDeleteDay}
+            onChangeStartDate={handleChangeStartDate}
+            onChangeEndDate={handleChangeEndDate}
+            onChangeStartHour={handleChangeStartTime}
+            onChangeEndHour={handleChangeEndTime}
+          />
+        )}
+        <button onClick={handleAddDay}>Add day</button>
+      </section>
+      <section
+        style={{
+          position: "relative",
+          display: "flex",
+          justifyContent: "center",
+          width: "50%",
+        }}
+      >
+        <code
+          style={{
+            position: "sticky",
+            top: "3rem",
+            maxWidth: "80%",
+            whiteSpace: "normal",
+            padding: "1.5rem",
+            height: "fit-content",
+          }}
+        >
+          {JSON.stringify(state.context, undefined, 8)}
+        </code>
+      </section>
+    </section>
   );
 }
